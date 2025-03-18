f75db8e5da5782ebcd56095e07fcf438ed107541
arfan4-dev <arfan4.dev@gmail.com>
2025-03-18 16:57:29 +0500

    feat: add new assets and improve search input styling; update sidebar width and modify VirtualOsceModal button label

--- src/components/Textbook.jsx
+++ src/components/Textbook.jsx
@@ -19,7 +19,6 @@ const Textbook = () => {
     setIsOpen((prevState) => !prevState);
   };
 
-
   const [query, setQuery] = useState("");
   const fuse = new Fuse(categoryName?.categoryName, {
     keys: ["categoryName"],
@@ -53,36 +52,24 @@ const Textbook = () => {
 
       <div className="w-full lg:ml-[250px]">
         {/* Main Content Section */}
-        <div className="flex-1 lg:p-8">
+        <div className="flex-1 lg:p-[16px]">
           {/* Back Button */}
           <div className="mb-2 flex items-center justify-end gap-x-5 p-2 lg:mb-5 lg:p-0">
-            {/* <button
-              onClick={() => navigate(-1)}
-              className="flex items-center gap-x-2 rounded-lg bg-white px-4 py-2 text-[12px] shadow-md hover:shadow-lg sm:text-[14px] lg:text-[16px]"
-            >
-              <FaAngleLeft color="#3CC8A1" />
-              <span className="font-semibold text-[#3CC8A1]">
-                Back To All Modules
-              </span>
-            </button> */}
-
             <div className="hidden justify-end lg:flex">
               <input
                 type="search"
                 placeholder="Search for anything"
-                className="w-full max-w-[400px] rounded-md p-2 pl-4 placeholder:text-[12px] placeholder:font-light"
+                className="w-full max-w-[400px] rounded-md p-2 pl-4 text-[14px] placeholder:text-[12px] placeholder:font-light"
                 value={query}
                 onChange={(e) => setQuery(e.target.value)} // Update query state on input change
+                style={{
+                  WebkitAppearance: "none", // Safari ke liye
+                  MozAppearance: "none", // Firefox ke liye
+                  appearance: "none",
+                }}
               />
             </div>
           </div>
-
-          {/* Title */}
-          {/* <h1 className="px-4 text-[20px] font-bold text-[#27272A] sm:text-[20px] lg:p-0 lg:text-[24px]">
-            Acute and Emergency
-          </h1> */}
-
-          {/* Search Bar */}
 
           {/* Categories List */}
           <div className="mt-6">
@@ -104,30 +91,6 @@ const Textbook = () => {
             </ul>
           </div>
         </div>
-
-        {/* <div
-          className="fixed bottom-5 right-5"
-          value={query}
-          onChange={(e) => setQuery(e.target.value)}
-        >
-          <div className="cursor-pointer rounded-[4px] bg-[#3CC8A1] p-2 text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              fill="none"
-              stroke="currentColor"
-              stroke-width="2"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              class="lucide lucide-search"
-            >
-              <circle cx="11" cy="11" r="8" />
-              <path d="m21 21-4.3-4.3" />
-            </svg>
-          </div>
-        </div> */}
       </div>
       <MobileBar
         toggleDrawer={toggleDrawer}
